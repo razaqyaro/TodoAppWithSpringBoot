c85efccd8ad855a914e99986ee0a2acd1d0a12cf
Abdul-Razak Hussein <abdul-razak.hussein@amalitech.com>
2023-07-18 16:34:32 +0000

    Add sayhello controller

--- src/main/java/com/razakspringboot/firstapp/ToDoAppApplication.java
+++ src/main/java/com/razakspringboot/firstapp/ToDoAppApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ToDoAppApplication {
+public class ToDoAppApplication
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		SpringApplication.run(ToDoAppApplication.class, args);
 	}
 
---
+++ src/main/java/com/razakspringboot/firstapp/hello/SayHelloController.java
@@ -0,0 +1,4 @@
+package com.razakspringboot.firstapp.hello;
+
+public class SayHelloController {
+}
