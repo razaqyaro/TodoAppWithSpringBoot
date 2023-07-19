07981e339de8b053387c01611125b14f5f29b731
Abdul-Razak Hussein <abdul-razak.hussein@amalitech.com>
2023-07-19 11:04:25 +0000

    Add TodoService and create static todos

--- src/main/java/com/razakspringboot/firstapp/todo/TodoService.java
+++ src/main/java/com/razakspringboot/firstapp/todo/TodoService.java
@@ -1,4 +1,48 @@
 package com.razakspringboot.firstapp.todo;
 
-public class TodoService {
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+public class TodoService
+{
+    private static List<Todo> todos;
+
+    static {
+        todos.add(new Todo(1,
+                "Abdul-Razak",
+                "Learn AWS",
+                LocalDate.now().plusYears(1),
+                false
+
+        ));
+        todos.add(new Todo(1,
+                "Mohammad Ali",
+                "Learn Full Stack Development",
+                LocalDate.now().plusYears(2),
+                false
+
+        ));
+        todos.add(new Todo(1,
+                "Agya Dan",
+                "Html",
+                LocalDate.now().plusYears(3),
+                false
+
+        ));
+        todos.add(new Todo(1,
+                "Christopher Owusu",
+                "Agile Testing",
+                LocalDate.now().plusYears(2),
+                false
+
+        ));
+    }
+
+    public List<Todo> findByUsername(String username)
+    {
+        return todos;
+    }
 }
